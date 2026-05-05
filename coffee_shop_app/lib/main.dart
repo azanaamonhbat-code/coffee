9127f25a2f8abf71c08637bf2db491340400d6c8
Таны нэр <azanaamonhbat@gmail.com>
2026-05-05 14:39:31 +0800

    liquid

--- coffee_shop_app/lib/main.dart
+++ coffee_shop_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'dart:ui' as ui;
 
 import 'pages/home_page.dart';
 import 'pages/orders_page.dart';
@@ -16,6 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Coffee Shop',
       home: LoginPage(),
     );
   }
@@ -34,12 +37,14 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int selectedIndex = 0;
+  late final LiquidController _liquidController;
 
   late final List<Widget> pages;
 
   @override
   void initState() {
     super.initState();
+    _liquidController = LiquidController();
 
     pages = [
       const HomePage(),
@@ -52,58 +57,34 @@ class _MainPageState extends State<MainPage> {
     setState(() {
       selectedIndex = index;
     });
-  }
-
-  // 🔥 CUSTOM NAV ITEM
-  Widget navItem(IconData icon, String label, int index) {
-    bool isSelected = selectedIndex == index;
-
-    return GestureDetector(
-      onTap: () => onItemTapped(index),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 250),
-        padding: EdgeInsets.symmetric(
-          horizontal: isSelected ? 16 : 10,
-          vertical: 8,
-        ),
-        decoration: BoxDecoration(
-          // 🔥 FIX: бүх item нэг бараан background-той боллоо
-          color: isSelected
-              ? Colors.brown.shade400
-              : const Color(0xFF1C1814),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
-          children: [
-            Icon(
-              icon,
-              color: isSelected ? Colors.white : Colors.grey,
-              size: 24,
-            ),
-            if (isSelected) ...[
-              const SizedBox(width: 8),
-              Text(
-                label,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ]
-          ],
-        ),
-      ),
-    );
+    _liquidController.animateToPage(page: index);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[selectedIndex],
+      extendBody: true,
+      body: LiquidSwipe(
+        pages: pages,
+        liquidController: _liquidController,
+        fullTransitionValue: 600,
+        enableSideReveal: true,
+        enableLoop: false,
+        waveType: WaveType.liquidReveal,
+        positionSlideIcon: 0.5,
+        slideIconWidget: const Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.white70,
+          size: 28,
+        ),
+        onPageChangeCallback: (index) {
+          setState(() => selectedIndex = index);
+        },
+      ),
 
-      // 🔥 MODERN FLOATING NAVBAR
+      // Modern Floating Bottom Navigation
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 20),
         child: Container(
           height: 70,
           decoration: BoxDecoration(
@@ -111,8 +92,8 @@ class _MainPageState extends State<MainPage> {
             borderRadius: BorderRadius.circular(30),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.4),
-                blurRadius: 20,
+                color: Colors.black.withOpacity(0.5),
+                blurRadius: 25,
                 offset: const Offset(0, 10),
               ),
             ],
@@ -125,6 +106,45 @@ class _MainPageState extends State<MainPage> {
               navItem(Icons.person_rounded, "Профайл", 2),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget navItem(IconData icon, String label, int index) {
+    bool isSelected = selectedIndex == index;
+
+    return GestureDetector(
+      onTap: () => onItemTapped(index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: EdgeInsets.symmetric(
+          horizontal: isSelected ? 18 : 12,
+          vertical: 10,
+        ),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.brown.shade400 : Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? Colors.white : Colors.grey,
+              size: 26,
+            ),
+            if (isSelected) ...[
+              const SizedBox(width: 8),
+              Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15,
+                ),
+              ),
+            ]
+          ],
         ),
       ),
     );
